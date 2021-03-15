8935eced6b103f8b42978e4ed1fe988322f4587c
Hưng Nguyễn <tathung.94.2014@gmail.com>
2021-03-15 19:52:26 +0700

    Update manifest

--- app/src/main/java/com/example/image_management/MainActivity.java
+++ app/src/main/java/com/example/image_management/MainActivity.java
@@ -1,28 +1,131 @@
 package com.example.image_management;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
-import com.example.camera_application.R;
+import com.example.image_management.R;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 import static java.sql.DriverManager.println;
 
 public class MainActivity extends AppCompatActivity {
+    String currentPhotoPath;
+    int REQUEST_IMAGE_CAPTURE = 1;
+    int CAMERA_PERM_CODE=101;
+    CardView camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
     }
+    private void askCameraPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.CAMERA},CAMERA_PERM_CODE);
+        }
+        else{
+            openCamera();
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==CAMERA_PERM_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                openCamera();
+            }
+            else{
+                Toast.makeText(this, "Camera permission is required!",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            System.out.println(imageBitmap);
+//            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+    private void openCamera() {
+        Intent open = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(open,REQUEST_IMAGE_CAPTURE);
+//        startActivity(open);
+
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        // Ensure that there's a camera activity to handle the intent
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            // Create the File where the photo should go
+//            File photoFile = null;
+//            try {
+//                photoFile = createImageFile();
+//            } catch (IOException ex) {
+//                // Error occurred while creating the File
+//            }
+//            // Continue only if the File was successfully created
+//            if (photoFile != null) {
+//                Uri photoURI = FileProvider.getUriForFile(this,
+//                        "com.example.android.fileprovider",
+//                        photoFile);
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//            }
+//        }
+    }
     public void main_menu_onclick(View view) {
-        switch ((view.getId())) {
+        switch (view.getId()) {
             case R.id.mn_camera: {
+                askCameraPermission();
                 break;
             }
-
             case R.id.mn_setting: {
                 setContentView(R.layout.setting);
                 break;
@@ -31,12 +134,12 @@ public class MainActivity extends AppCompatActivity {
                 setContentView(R.layout.password);
                 break;
             }
-            case R.id.mn_album: {
-                break;
-            }
-            case R.id.mn_archive: {
-                break;
-            }
+//            case R.id.mn_album: {
+//                break;
+//            }
+//            case R.id.mn_archive: {
+//                break;
+//            }
             case R.id.mn_theme: {
                 setContentView(R.layout.theme);
                 break;
@@ -45,7 +148,6 @@ public class MainActivity extends AppCompatActivity {
                 setContentView(R.layout.main_menu);
                 break;
             }
-
         }
     }
 }
