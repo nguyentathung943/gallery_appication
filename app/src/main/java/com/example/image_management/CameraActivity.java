716b8d85c6d49643bf6a4855c5af45d64109cefd
nguyentathung943 <tathung.94.2014@gmail.com>
2021-03-31 15:11:03 +0700

    merge

--- app/src/main/java/com/example/image_management/CameraActivity.java
+++ app/src/main/java/com/example/image_management/CameraActivity.java
@@ -122,7 +122,6 @@ public class CameraActivity extends AppCompatActivity {
         currentPhotoPath = image.getAbsolutePath();
         return image;
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
